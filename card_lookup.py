2375039d654d07fdd1c8420998536b6cafe8b5ab
Zhao Tang <zxt@zhaotang.ca>
2019-08-22 18:57:05 -0400

    refactor into more functions for easier testing

--- card_lookup.py
+++ card_lookup.py
@@ -21,6 +21,17 @@ def clean_disc_string(string):
                             cleaned_string)
     return cleaned_string
 
+
+def process_exact_match(group, cur):
+    exact_sql = 'SELECT * FROM cards WHERE card_name = ? COLLATE NOCASE'
+    return cur.execute(exact_sql, [group]).fetchone()
+
+
+def process_card_id_match(group, cur):
+    id_sql = 'SELECT * FROM cards WHERE card_id = ?'
+    return cur.execute(id_sql, [group]).fetchone()
+
+
 def process_nonexact_match(input_group, current_cursor):
     non_exact_sql = "SELECT * FROM cards WHERE card_name LIKE ?"
 
@@ -46,6 +57,52 @@ def process_nonexact_match(input_group, current_cursor):
         return "Error"
 
 
+def generate_reply_message(results, cur):
+    reply_message = ""
+    if results:
+        card_ids = []
+        for r in results:
+            # don't make dupe replies to same card
+            if r['card_id'] in card_ids:
+                continue
+            card_ids.append(r['card_id'])
+            cleaned_skill_disc = clean_disc_string(r['skill_disc'])
+            if(r['evo_skill_disc'] and
+               r['evo_skill_disc'] != r['skill_disc']):
+                cleaned_evo_disc = clean_disc_string(r['evo_skill_disc'])
+                t = templates.EVO_SKILL_DISC_TEMPLATE_FRAG
+                cleaned_skill_disc = ''.join([cleaned_skill_disc,
+                                             t.format(cleaned_evo_disc)])
+
+            r['skill_disc'] = cleaned_skill_disc
+            r['stats'] = str(r['cost']) + 'pp'
+            if r['char_type'] == 1:
+                r['stats'] = ''.join([r['stats'], ' ',
+                                      str(r['atk']), '/', str(r['life']),
+                                      ' -> ',
+                                      str(r['evo_atk']), '/',
+                                      str(r['evo_life'])])
+
+            r['craft'] = lookup_name_from_id(r['clan'], 'crafts', cur)
+            r['card_rarity'] = lookup_name_from_id(r['rarity'],
+                                                   'card_rarity', cur)
+            r['card_type'] = lookup_name_from_id(r['char_type'],
+                                                 'card_types', cur)
+            r['card_set'] = lookup_name_from_id(r['card_set_id'],
+                                                'card_sets', cur)
+
+            r['art_links'] = templates.BASE_ART_LINK.format(r['card_id'])
+            if r['card_type'] == 'Follower':
+                t = templates.EVO_ART_LINK
+                r['art_links'] = ''.join([r['art_links'],
+                                          t.format(r['card_id'])])
+
+            reply_message = ''.join([reply_message,
+                                     templates.CARD_TEMPLATE.format(**r)])
+
+        return reply_message
+
+
 def process_card_lookup(matches):
     with DBConnect(settings.CARD_DB) as conn:
         conn.set_trace_callback(logging.info)
@@ -55,15 +112,14 @@ def process_card_lookup(matches):
         for match in matches:
             for group in match:  # a 2-tuple, [[group 1]] and \[\[group2\]\]
                 if not group:
-                  continue
+                    continue
 
                 # try to search exact matches
-                exact_sql = 'SELECT * FROM cards WHERE card_name = ? COLLATE NOCASE'
-                row = cur.execute(exact_sql, [group]).fetchone()
+                row = process_exact_match(group, cur)
                 if(row is not None):
                     results.append(row)
 
-                else: # try to search for non-exact matches
+                else:  # try to search for non-exact matches
                     row = process_nonexact_match(group, cur)
                     if row == "Error":
                         continue
@@ -71,51 +127,8 @@ def process_card_lookup(matches):
                         results.append(row)
 
                     else:  # try to search by card ID
-                        id_sql = 'SELECT * FROM cards WHERE card_id = ?'
-                        row = cur.execute(id_sql, [group]).fetchone()
+                        row = process_card_id_match(group, cur)
                         if(row is not None):
                             results.append(row)
 
-        reply_message = ""
-        if results:
-            card_ids = []
-            for r in results:
-                # don't make dupe replies to same card
-                if r['card_id'] in card_ids:
-                    continue
-                card_ids.append(r['card_id'])
-                cleaned_skill_disc = clean_disc_string(r['skill_disc'])
-                if(r['evo_skill_disc'] and
-                   r['evo_skill_disc'] != r['skill_disc']):
-                    cleaned_evo_disc = clean_disc_string(r['evo_skill_disc'])
-                    t = templates.EVO_SKILL_DISC_TEMPLATE_FRAG
-                    cleaned_skill_disc = ''.join([cleaned_skill_disc,
-                                                 t.format(cleaned_evo_disc)])
-
-                r['skill_disc'] = cleaned_skill_disc
-                r['stats'] = str(r['cost']) + 'pp'
-                if r['char_type'] == 1:
-                    r['stats'] = ''.join([r['stats'], ' ',
-                                          str(r['atk']), '/', str(r['life']),
-                                          ' -> ',
-                                          str(r['evo_atk']), '/',
-                                          str(r['evo_life'])])
-
-                r['craft'] = lookup_name_from_id(r['clan'], 'crafts', cur)
-                r['card_rarity'] = lookup_name_from_id(r['rarity'],
-                                                       'card_rarity', cur)
-                r['card_type'] = lookup_name_from_id(r['char_type'],
-                                                     'card_types', cur)
-                r['card_set'] = lookup_name_from_id(r['card_set_id'],
-                                                    'card_sets', cur)
-
-                r['art_links'] = templates.BASE_ART_LINK.format(r['card_id'])
-                if r['card_type'] == 'Follower':
-                    t = templates.EVO_ART_LINK
-                    r['art_links'] = ''.join([r['art_links'],
-                                              t.format(r['card_id'])])
-
-                reply_message = ''.join([reply_message,
-                                         templates.CARD_TEMPLATE.format(**r)])
-
-        return reply_message
+        return generate_reply_message(results, cur)
